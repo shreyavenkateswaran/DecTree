771313622a70f87f61c0eaaa8638f796f6499128
shreyavenkateswaran <76979793+shreyavenkateswaran@users.noreply.github.com>
2021-07-23 14:56:48 -0400

    Update HospitalAppointments.js

--- HospitalAppointments.js
+++ HospitalAppointments.js
@@ -12,7 +12,7 @@ var AnimationEvents = MindFusion.Animations.Events;
 var str = [];
 var n;
 var obj;
-// let stack = [];
+let stack = [];
 var arr = [];
 var zero = [];
 var vec;
@@ -20,8 +20,8 @@ var bx = 50, by = 40;
 var diagram = null;
 
 // $(document).ready(function () {
-// 	diagram = Diagram.create(document.getElementById("diagram"));
-// 	diagram.setBounds(new Rect(0, 0, 500, 500));
+//  diagram = Diagram.create(document.getElementById("diagram"));
+//  diagram.setBounds(new Rect(0, 0, 500, 500));
 // });
 
 // console.log(str);
@@ -29,61 +29,61 @@ var diagram = null;
 var input1 = document.querySelector('input');
 var textarea = document.querySelector('textarea');
 input1.addEventListener('change', () => {
-	let files = input1.files;
-	if (files.length == 0) return;
+    let files = input1.files;
+    if (files.length == 0) return;
 
-	const file = files[0];
-	let reader = new FileReader();
-	reader.onload = (e) => {
-		const file = e.target.result;
-		const lines = file.split(/\r\n|\n/);
-		textarea.value = lines.join('\n');
-	};
-	reader.onerror = (e) => alert(e.target.error.name);
+    const file = files[0];
+    let reader = new FileReader();
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        textarea.value = lines.join('\n');
+    };
+    reader.onerror = (e) => alert(e.target.error.name);
 
-	reader.readAsText(file);
+    reader.readAsText(file);
 });
 function input() {
-	// document.getElementById("undo").style.display = "inline-block";
-	str = $('#input').val().split("\n");
-	n = str.length;
-	vec = new Array(n);
-	obj = new Array(n);
-	for (var i = 0; i < obj.length; i++) {
-		obj[i] = new Array(4);
-		arr[i] = new Array(0);
-	}
+    // document.getElementById("undo").style.display = "inline-block";
+    str = $('input').val().split("\n");
+    n = str.length;
+    vec = new Array(n);
+    obj = new Array(n);
+    for (var i = 0; i < obj.length; i++) {
+        obj[i] = new Array(4);
+        arr[i] = new Array(0);
+    }
 
-	console.log(n);
+     console.log(n);
 
-	let p = 3;
-	// console.log(str[1]);
-	for (var i = 0; i < n; i++) {
-		let size = 0;
-		for (var j = 0; j < str[i].length; j++) {
-			if (str[i][j] != '-') break;
-			else size++;
-		}
-		// console.log(size);
-		vec[size / 2] = i;
-		if (size == 0) {
-			p++;
-			// arr[0].push(i);
-			zero.push(i);
-		}
-		else {
-			arr[vec[(size / 2) - 1]].push(i);
-		}
-		// console.log(size);
-		let len = str[i].length;
-		// let s = str[i].search(',');
-		str[i] = str[i].substring(size);
-		obj[size / 2].push(str[i]);
-	}
+    let p = 3;
+    // console.log(str[1]);
+    for (var i = 0; i < n; i++) {
+        let size = 0;
+        for (var j = 0; j < str[i].length; j++) {
+            if (str[i][j] != '-') break;
+            else size++;
+        }
+        // console.log(size);
+        vec[size / 2] = i;
+        if (size == 0) {
+            p++;
+            // arr[0].push(i);
+            zero.push(i);
+        }
+        else {
+            arr[vec[(size / 2) - 1]].push(i);
+        }
+        // console.log(size);
+        let len = str[i].length;
+        // let s = str[i].search(',');
+        str[i] = str[i].substring(size);
+        obj[size / 2].push(str[i]);
+    }
 
-	console.log(arr);
-	localStorage.setItem("str-array", JSON.stringify(str));
-	localStorage.setItem("arr-array", JSON.stringify(arr));
+    console.log(arr);
+    localStorage.setItem("str-array", JSON.stringify(str));
+    localStorage.setItem("arr-array", JSON.stringify(arr));
 
-	document.location.href = "tree.html";
+    document.location.href = "tree.html";
 }
